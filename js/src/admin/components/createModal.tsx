0fceb1544707d32dbd8909d953f8b9c2ebd41422
FoskyM <i@fosky.top>
2025-05-08 14:40:43 +0800

    perf: create modal

--- js/src/admin/components/createModal.tsx
+++ js/src/admin/components/createModal.tsx
@@ -16,54 +16,68 @@ export default class createModal extends Modal {
         return "Modal Modal--large"
     }
     title() {
-        return "Create Virtual Item"
+        return _trans('title')
     }
     content() {
-        return <div className="Modal-body">
-            <div className="Form">
-                <div className="Form-group">
-                    <label for="xypp-virtual-item-ipt-name">{_trans('name')}</label>
-                    <input id="xypp-virtual-item-ipt-name" required className="FormControl" type="text" step="any" value={this.name} onchange={((e: InputEvent) => {
-                        this.name = (e.target as HTMLInputElement).value;
-                    }).bind(this)} />
-                </div>
-                <Button className="Button Button--primary" onclick={this.click.bind(this)} disabled={this.loading} loading={this.loading}>{_trans("submit")}</Button>
-                <br /><br />
-                <textarea id="xypp-virtual-item-ipt-data-convert" className="FormControl" rows={3}></textarea>
-                <Button className="Button" onclick={this.convert.bind(this)}>{_trans("convert")}</Button>
-                <table className="Table Table--full">
-                    <thead>
-                        <tr>
-                            <th>{_trans('key')}</th>
-                            <th>{_trans('content')}</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.data.map((item, index) => {
-                            return <tr key={index}>
-                                <td><input className="FormControl" type="text" value={item.key} onchange={((e: InputEvent) => {
-                                    this.data[index].key = (e.target as HTMLInputElement).value;
-                                }).bind(this)} /></td>
-                                <td><input className="FormControl" type="text" value={item.content} onchange={((e: InputEvent) => {
-                                    this.data[index].content = (e.target as HTMLInputElement).value;
-                                }).bind(this)} /></td>
-                                <td>
-                                    <Button onclick={(() => {
-                                        this.data.splice(index, 1);
-                                        m.redraw();
-                                    }).bind(this)}>
-                                        <i className="fas fa-trash"></i>
+        return <>
+            <div className="Modal-body">
+                <div className="Form">
+                    <div className="Form-group">
+                        <label for="xypp-virtual-item-ipt-name">{_trans('name')}</label>
+                        <input id="xypp-virtual-item-ipt-name" required className="FormControl" type="text" step="any" value={this.name} onchange={((e: InputEvent) => {
+                            this.name = (e.target as HTMLInputElement).value;
+                        }).bind(this)} />
+                    </div>
+
+                    <div className="Form-group">
+                        <textarea id="xypp-virtual-item-ipt-data-convert" className="FormControl" rows={3}></textarea>
+                        <Button className="Button" onclick={this.convert.bind(this)}>{_trans("convert")}</Button>
+                    </div>
+
+                    <table className="Table Table--full">
+                        <thead>
+                            <tr>
+                                <th>{_trans('key')}</th>
+                                <th>{_trans('content')}</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.data.map((item, index) => {
+                                return <tr key={index}>
+                                    <td><input className="FormControl" type="text" value={item.key} onchange={((e: InputEvent) => {
+                                        this.data[index].key = (e.target as HTMLInputElement).value;
+                                    }).bind(this)} /></td>
+                                    <td><input className="FormControl" type="text" value={item.content} onchange={((e: InputEvent) => {
+                                        this.data[index].content = (e.target as HTMLInputElement).value;
+                                    }).bind(this)} /></td>
+                                    <td>
+                                        <Button className="Button Button--danger" onclick={(() => {
+                                            this.data.splice(index, 1);
+                                            m.redraw();
+                                        }).bind(this)}>
+                                            <i className="fas fa-trash"></i>
+                                        </Button>
+                                    </td>
+                                </tr>
+                            })}
+                            <tr>
+                                <td colspan={3}>
+                                    <Button className="Button Button--block" onclick={this.addRow.bind(this)}>
+                                        <i className="fas fa-plus"></i> {_trans("add")}
                                     </Button>
                                 </td>
                             </tr>
-                        })}
-                        <tr><td><Button className="Button Button--primary" onclick={this.addRow.bind(this)}>{_trans("add")}</Button></td></tr>
-                    </tbody>
-                </table>
+                        </tbody>
+                    </table>
+                </div>
             </div>
-        </div >
+            <div className="Modal-footer">
+                <Button className="Button Button--primary" onclick={this.click.bind(this)} disabled={this.loading} loading={this.loading}>{_trans("submit")}</Button>
+            </div>
+        </>
     }
+
     click() {
         for (let i = 0; i < this.data.length; i++) {
             if (!this.data[i].key) {
@@ -99,7 +113,7 @@ export default class createModal extends Modal {
         val.trim().split("\n").forEach(line => {
             const text = line.trim();
             if (!text) return;
-            const pos = (text.indexOf("\t") + 1 || text.indexOf(",") + 1 || text.indexOf(" ") + 1) - 1;
+            const pos = (text.indexOf("\t") + 1 || text.indexOf(",") + 1 || text.indexOf(" ") + 1 || text.indexOf(":") + 1) - 1;
             if (pos != -1) {
                 this.data.push({
                     key: text.substring(0, pos),
